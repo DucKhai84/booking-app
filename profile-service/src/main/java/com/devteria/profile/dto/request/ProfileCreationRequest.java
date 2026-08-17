1690a4310e1cc6c1e7aee936b94520d40a4671df
Duc Khai <hokhai84@gmail.com>
2026-08-17 11:22:00 +0700

    feat: implementation profile service

---
+++ profile-service/src/main/java/com/devteria/profile/dto/request/ProfileCreationRequest.java
@@ -0,0 +1,17 @@
+package com.devteria.profile.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProfileCreationRequest {
+    String firstName;
+    String lastName;
+    String city;
+}
---
+++ profile-service/src/main/java/com/devteria/profile/entity/UserProfile.java
@@ -0,0 +1,28 @@
+package com.devteria.profile.entity;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
+import org.springframework.data.neo4j.core.schema.Node;
+import org.springframework.data.neo4j.core.support.UUIDStringGenerator;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Node("user_profile")
+public class UserProfile {
+    @GeneratedValue(generatorClass = UUIDStringGenerator.class)
+    @Id
+    String id;
+
+    String userId;
+    String firstName;
+    String lastName;
+    String city;
+}
