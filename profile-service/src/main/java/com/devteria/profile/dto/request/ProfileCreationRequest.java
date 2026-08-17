8dfaeb3cf94c07fa249fbe30840c4fd860deb86f
Duc Khai <hokhai84@gmail.com>
2026-08-17 14:29:11 +0700

    feat: initial-add-openfeign

---
+++ profile-service/src/main/java/com/devteria/profile/dto/request/ProfileCreationRequest.java
@@ -0,0 +1,18 @@
+package com.devteria.profile.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ProfileCreationRequest {
+    String userId;
+    String firstName;
+    String lastName;
+    String city;
+}
