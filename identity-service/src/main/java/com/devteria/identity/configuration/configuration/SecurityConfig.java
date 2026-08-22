e6d214ef6f2e4140a35f4c48fdf2c87a1b811274
Duc Khai <hokhai84@gmail.com>
2026-08-22 13:43:37 +0700

    feat: implementation done add authorization

--- identity-service/src/main/java/com/devteria/identity/configuration/configuration/SecurityConfig.java
+++ identity-service/src/main/java/com/devteria/identity/configuration/configuration/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.devteria.identity.configuration;
+package com.devteria.identity.configuration.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
